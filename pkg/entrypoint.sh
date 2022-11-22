fc85f1ab168781241a75a6166df345724b7bee07
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-11-22 23:53:55 +0200

    Added connection string builder and added env vars

--- pkg/entrypoint.sh
+++ pkg/entrypoint.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 set -e
 
+
 if [ -z "$PSQL_CONNECTION_STRING" ]
 then
-    echo "No psql connection string provided!"
-    exit 1
+    if [ ! -z "$PSQL_HOST" ]
+    then
+        $PSQL_CONNECTION_STRING = "$PSQL_CONNECTION_STRING host=$PSQL_HOST"
+    fi
+    if [ ! -z "$PSQL_PORT" ]
+    then
+        $PSQL_CONNECTION_STRING = "$PSQL_CONNECTION_STRING port=$PSQL_PORT"
+    fi
+    if [ ! -z "$PSQL_SSLMODE" ]
+    then
+        $PSQL_CONNECTION_STRING = "$PSQL_CONNECTION_STRING sslmode=$PSQL_SSLMODE"
+    fi
+    if [ ! -z "$PSQL_USER" ]
+    then
+        $PSQL_CONNECTION_STRING = "$PSQL_CONNECTION_STRING user=$PSQL_USER"
+    fi
+    if [ ! -z "$PSQL_PASSWORD" ]
+    then
+        $PSQL_CONNECTION_STRING = "$PSQL_CONNECTION_STRING password=$PSQL_PASSWORD"
+    fi
+    if [ ! -z "$PSQL_DBNAME" ]
+    then
+        $PSQL_CONNECTION_STRING = "$PSQL_CONNECTION_STRING sslmode=$PSQL_DBNAME"
+    fi
 fi
+
 if [ -z "$PSQL_SCRIPT" ]
 then
     if [ -z "$PSQL_SCRIPT_FILE" ]
@@ -16,4 +40,6 @@ then
     psql "$PSQL_CONNECTION_STRING" -f $PSQL_SCRIPT_FILE
     exit 0
 fi
-psql "$PSQL_CONNECTION_STRING" -c "$PSQL_SCRIPT"
+
+psql "$PSQL_CONNECTION_STRING" -c "$PSQL_SCRIPT" \
+    || psql --help && exit 1
